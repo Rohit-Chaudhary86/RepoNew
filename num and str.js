b164bed9610776142251b85eed6a4cb1741d7401
Rohit-Chaudhary86 <151979116+Rohit-Chaudhary86@users.noreply.github.com>
2024-01-28 16:09:33 +0530

    Add files via upload

---
+++ num and str.js
@@ -0,0 +1,8 @@
+let firstname="rohit"
+let lastname="chaudhary"
+console.log( firstname +' '+ lastname)
+console.log(`my name is ${firstname} ${lastname}`)
+
+const username=new String("rohitdotcom")
+console.log(username[0]);
+console.log(username.length);
